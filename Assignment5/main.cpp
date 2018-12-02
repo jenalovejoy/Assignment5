2050a0684e60cc584cc2c14dfd3f46d4a33bc235
Jena Lovejoy <jena.lovejoy@mac.com>
2018-12-02 14:04:43 -0800

    Initial Commit

---
+++ Assignment5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment5
+//
+//  Created by Jena Lovejoy on 12/2/18.
+//  Copyright © 2018 Jena Lovejoy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
